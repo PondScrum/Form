d84f495f492cd7d046af1dcd038bdb5eee01fcfd
Parker <parkergagliano12@gmail.com>
2025-02-17 20:05:28 +0000

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+import svelte from "rollup-plugin-svelte";
+import resolve from "@rollup/plugin-node-resolve";
+import pkg from './package.json' with { type: 'json' }
+console.log(pkg)
+export default {
+    input: "src/Form.svelte",
+    output: [
+        { file: pkg.module, format: "es" },
+        { file: pkg.main, format: "umd", name: "Name" },
+    ],
+    plugins: [svelte(), resolve()],
+};
